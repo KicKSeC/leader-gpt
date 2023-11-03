f7ccb5718a97c225eb0656f393af7635572cb72b
JunHyeong Park <wist2483@gmail.com>
2023-11-03 21:59:31 +0900

    231103
    try except in chatgpt, block more answer while answering add show_token command

--- chatgpt.py
+++ chatgpt.py
@@ -11,7 +11,7 @@ system_role = {"role": "system",
                "content": "You are a supportive team leader and enabler of your teammates' goals."}
 
 token_usage = 0     # 사용된 총 토큰 수 
-def increase_token_usage(tokens):
+def _increase_token_usage(tokens):
     global token_usage
     token_usage += tokens
     logging.info('token increased')
@@ -23,7 +23,7 @@ def get_response_object(message, model=gpt_model["gpt3.5"], user='user'):
         model=model, messages=[system_role, {"role": user, "content": message}]
     )
     
-    logging.info('gettnig token usage')
+    logging.info('getting token usage')
     increase_token_usage(response.usage.total_tokens)
     
     return response
@@ -31,16 +31,19 @@ def get_response_object(message, model=gpt_model["gpt3.5"], user='user'):
 # 응답 객체의 메세지 내용
 def get_response(message, model=gpt_model, user='user'):
     logging.info('returning message')
-    return get_response_object(message, model=model, user=user).choices[0].message.content
+    try:
+        return get_response_object(message, model=model, user=user).choices[0].message.content
+    except openai.error.InvalidRequestError as e:
+        logging.error(e)
+        return '오류가 일어나 대답할 수 없어요 ㅠㅠ'
     # try: 
     #     return get_response_object(message, user=user).choice[0].message.content
     # except Exception as e:
     #     print(e)
     #     return "에러 발생!"
 
-def show_token_usage():
-    logging.info('showing token usage')
-    print(token_usage)
+def get_token_usage(): 
+    return token_usage
 
 # TODO 이어서 대화하기, 대화 끊기, 여러 개 동시에 대화하기 
 
--- discord_bot.py
+++ discord_bot.py
@@ -4,7 +4,7 @@ import discord
 import chatgpt
 
 BOT_TOKEN = os.getenv('DISCORD_TOKEN')
-CHANNEL_ID = int(os.getenv('TEAMPROJECT_CHANNEL_ID'))
+CHANNEL_ID = int(os.getenv('PLOEI_CHANNEL_ID'))
 DESCRIPTION = 'leader gpt can help your team!'
 GPT_ANSWER_CHECK = '✔'
 
@@ -12,8 +12,10 @@ GPT_ANSWER_CHECK = '✔'
 class LeaderClient(discord.Client):
     async def on_ready(self):
         #
-        self.command_keywords = { 'hello': self.say_hello, 'dice': self.roll, '!': self.gpt4_answer }
+        self.command_keywords = { 'hello': self.say_hello, 'dice': self.roll, '!': self.gpt4_answer,
+                                 'token': self.get_token_usage}
         self.channel = self.get_channel(CHANNEL_ID)
+        self.is_answering = False
 
         if self.channel is None:
             raise AttributeError(f"Channel {CHANNEL_ID} is not found")
@@ -21,7 +23,8 @@ class LeaderClient(discord.Client):
         await self.change_presence(status=discord.Status.online,
                                    activity=discord.Game("성실"))
         print(f'Logged on as {self.user}') 
-        await self.channel.send('Commands: ' + ', '.join(self.command_keywords.keys()))
+        await self.channel.send('Commands: ' + ', '.join(self.command_keywords.keys()) +
+                                '. 예시: !hello')
         
     async def on_message(self, message):
         if message.author == self.user: # ignore message of itself
@@ -39,9 +42,15 @@ class LeaderClient(discord.Client):
                 await self.command_keywords[keyword](message)   
                 return
         # if it's not command, then get response from chatgpt on chatgpt.py 
+        if self.is_answering:
+            await self.channel.send('이전 답변이 완료될 때까지 기다려주세요')
+            return
+            
         await message.add_reaction(GPT_ANSWER_CHECK)
+        self.is_answering = True
         answer = chatgpt.get_response(message.content)
         await self.channel.send(answer)     # send answer 
+        self.is_answering = False
     
     def trim_message_content(self, message) -> str:
         """trim command in message content"""
@@ -68,16 +77,23 @@ class LeaderClient(discord.Client):
             dice = self.trim_message_content(message)
             rolls, limit = map(int, dice.split('d'))
         except ValueError:
-            await self.channel.send('Format has to be in NdN!')
+            await self.channel.send('NdN 형식이 아니예요! 예시: !dice 2d20')
             return
 
         await self.channel.send(', '.join(str(random.randint(1, limit)) for r in range(rolls))) 
         
     async def gpt4_answer(self, message):
         content = self.trim_message_content(message)
+        if self.is_answering:
+            self.channel.send('이전 답변이 완료될 때까지 기다려주세요')
+            return
+        
         await message.add_reaction(GPT_ANSWER_CHECK)
         answer = chatgpt.get_response(content, model=chatgpt.gpt_model['gpt4'])
         await self.channel.send(answer)
+        
+    async def get_token_usage(self, message):
+        await self.channel.send(f'지금까지 사용된 토큰의 양은 {chatgpt.get_token_usage()}이에요.')
 
 
 intents = discord.Intents.default()
