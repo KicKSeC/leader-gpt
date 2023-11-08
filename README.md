23ea8a41abdcf98c7fd1f8e524163df5c87f029a
JunHyeong Park <wist2483@gmail.com>
2023-11-08 15:51:53 +0900

    README.md, chatgpt debug, email, gpt4-turbo
    readme.md 설명 추가, chatgpt 답변 불가 디버깅, email.py 추가, gpt4-turbo 업데이트

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 ﻿# 사용 세팅
-먼저 discord에 message intent를 포함한 기능들을 체크한 봇을 2OATH의 url 생성에서 bot체크, administer체크하여 url 복사
-이후 링크로 접속하여 봇을 추가할 서버를 선택한다. 그리고 설정에 가서 개발자 모드를 켠 다음에 봇이 채팅을 보낼 
+
+## 토큰을 저장하는 방법
+ChatGPT와 디스코드 봇의 보안을 유지하는 일은 중요합니다.
+이 파일이 있는 경로에 '.env'를 추가하고, 아래와 같이 입력합니다
+> OPENAI_API_KEY=[ChatGPT 키]
+> DISCORD_TOKEN=[디스코드 봇 키]
+그리고 os.getenv("OPENAI_API_KEY")와 같은 형식으로 코드에서 읽어들일 수 있습니다.
+
+필요한 데이터 목록
+- OPEN_API_KEY
+- DISCORD_TOKEN
--- chatgpt.py
+++ chatgpt.py
@@ -2,15 +2,15 @@
 import openai
 import logging
 
-openai.api_key = os.getenv('OPENAI_API_KEY') 
 logging.basicConfig(filename='chatgpt.log', filemode='w', level=logging.INFO ,
                     format='%(name)s - %(levelname)s - %(message)s')
+openai.api_key = os.getenv('OPENAI_API_KEY') 
 gpt_model = {"davinci": "text-davinch-003", "gpt3.5": "gpt-3.5-turbo", 
-             "babbage": "babbage-002", "gpt4": "gpt-4"}
+             "babbage": "babbage-002", "gpt4": "gpt-4", "gpt4-turbo": "gpt-4-1106-preview"}
 system_role = {"role": "system", 
                "content": "You are a supportive team leader and enabler of your teammates' goals."}
-
 token_usage = 0     # 사용된 총 토큰 수 
+
 def _increase_token_usage(tokens):
     global token_usage
     token_usage += tokens
@@ -29,13 +29,13 @@ def get_response_object(message, model=gpt_model["gpt3.5"], user='user'):
     return response
 
 # 응답 객체의 메세지 내용
-def get_response(message, model=gpt_model, user='user'):
+def get_response(message, model=gpt_model["gpt3.5"], user='user'):
     logging.info('returning message')
     try:
         return get_response_object(message, model=model, user=user).choices[0].message.content
     except openai.error.InvalidRequestError as e:
         logging.error(e)
-        return '오류가 일어나 대답할 수 없어요 ㅠㅠ'
+        return '답변에 오류가 발생하였습니다.'
     # try: 
     #     return get_response_object(message, user=user).choice[0].message.content
     # except Exception as e:
