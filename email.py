b0dd06a4be9e93f2ae83b752cdf7197d9864ef78
JunHyeong Park <wist2483@gmail.com>
2023-11-08 16:48:37 +0900

    email google 메일 추가

--- email.py
+++ email.py
@@ -53,11 +53,15 @@ def send(from_email, pwd, to_email, subject, content, file_path=None, server="na
         except smtplib.SMTPException:
             error_msg = ""
             error_msg += "잘못된 이메일 로그인 정보가 입력되었거나 허가되지 않은 접근입니다" 
+            error_msg += "\n허가되지 않은 접근의 경우 이를 허용하기 위해 아래 링크를 참조하십시오.\n" 
+
             if server == "naver":
-                error_msg += "\n허가되지 않은 접근의 경우 이를 허용하기 위해 아래 링크를 참조하십시오.\n" 
-                error_msg += "https://wikidocs.net/35963"
+                error_msg += "https://help.naver.com/service/5632/contents/18534?osType=PC&lang=ko"
+            elif server == "google":
+                error_msg += "https://support.google.com/a/answer/9003945?hl=ko 의 '이전 버전의 Outlook 및 기타 클라이언트로 Gmail 설정하기'"
 
             return error_msg
+
         if not response:
             return "메세지가 성공적으로 발송되었습니다"
         else:
