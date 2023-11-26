477d2db089b72fdcba31a3665882c5d59b743b11
Hangyeol Kang <benkang1624@gmail.com>
2023-11-26 20:49:24 +0900

    text  to embed

--- commands/schedule.py
+++ commands/schedule.py
@@ -9,7 +9,6 @@ class Schedule(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-
         self.next_meeting = ""
 
     @commands.group(name="회의시간")
@@ -18,11 +17,16 @@ class Schedule(commands.Cog):
         # 만약 서브 커맨드가 없다면, 다음 회의 시간을 출력하거나, 예정된 회의가 없다는 메시지를 출력합니다.
         if ctx.invoked_subcommand is None:
             if self.next_meeting:
-                await ctx.send(f"다음 회의는 {self.next_meeting}에 예정되어 있습니다.")
+                embed = discord.Embed(
+                    title="다음 회의 시간",
+                    description=f"다음 회의는 {self.next_meeting}에 예정되어 있습니다.",
+                    color=0x3498db
+                )
+                await ctx.send(embed=embed)
             else:
-                await ctx.send("예정된 회의가 없습니다.")
+                await ctx.send(embed=discord.Embed(description="예정된 회의가 없습니다.", color=0x3498db))
 
     @meeting.command(name="정하기")
     async def arrange_meeting(self, ctx):
         """사용자가 '회의시간 정하기' 명령어를 입력하면 실행되는 함수입니다. 회의 참석 가능 시간을 입력하도록 안내합니다."""
-        await ctx.send("본인의 참석가능한 시간을 입력해주세요")
+        await ctx.send(embed=discord.Embed(description="본인의 참석가능한 시간을 입력해주세요", color=0x3498db))
--- commands/team_rule.py
+++ commands/team_rule.py
@@ -13,45 +13,46 @@ class TeamRule(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-
         self.rules = []
 
     @commands.group(name="규칙")
     async def rule(self, ctx):
         """사용자가 '규칙' 명령어를 입력하면, 서브 커맨드가 없는 경우에 실행되는 함수입니다"""
         if ctx.invoked_subcommand is None:
-            await ctx.send("규칙 생성, 추가 혹은 확인을 선택해주세요 (예: !규칙 생성)")
+            embed = discord.Embed(
+                description="규칙 생성, 추가 혹은 확인을 선택해주세요 (예: !규칙 생성)",
+                color=0x3498db
+            )
+            await ctx.send(embed=embed)
 
     @rule.command(name="생성")
     async def create_rule(self, ctx):
         """사용자가 '규칙 생성' 명령어를 입력하면 실행되는 함수입니다. 규칙을 생성합니다."""
         if not ChatGPT.is_answering:  # 챗지피티가 이미 사용되고 있지는 않은지 확인
-            await ctx.send("ChatGPT를 통해 규칙생성")
+            await ctx.send(embed=discord.Embed(description="ChatGPT를 통해 규칙생성", color=0x3498db))
             rules = ChatGPT.get_response(message=PROMPT_CREATE_RULE)
-            await ctx.send(rules)
+            await ctx.send(embed=discord.Embed(description=rules, color=0x3498db))
 
             # 나열된 규칙을 개별적으로 나누어 (빈 거 제외하고) 각각 저장
             for rule in [x for x in rules.split('\n') if x != ""]:
                 self.rules.append(rule)
         else:
-            await ctx.send("이미 답변 중에 있습니다. 답변 이후에 요청해 주십시오.")
+            await ctx.send(embed=discord.Embed(description="이미 답변 중에 있습니다. 답변 이후에 요청해 주십시오.", color=0x3498db))
 
+    # 인덱스 붙이는 과정에서 문제 발생
     @rule.command(name="추가")
     async def append_rule(self, ctx, new_rule):
         """사용자가 '규칙 추가' 명령어를 입력하면 실행되는 함수입니다. 새로운 규칙을 추가하고, 추가된 규칙을 출력합니다"""
         self.rules.append(new_rule)
-        rules = ""
-        for idx, rule in enumerate(self.rules):
-            rules += f"{idx + 1}. {rule}\n"
-        await ctx.send("규칙이 추가되었습니다\n"
-                       f"현재 규칙\n"
-                       f"{rules}")
+        await ctx.send(embed=discord.Embed(description="규칙이 추가되었습니다", color=0x3498db))
 
     @rule.command(name="확인")
     async def check_rule(self, ctx):
         """사용자가 '규칙 확인' 명령어를 입력하면 실행되는 함수입니다. 현재 규칙을 출력합니다"""
+        if not self.rules:
+            await ctx.send(embed=discord.Embed(description="생성된 규칙이 존재하지 않습니다", color=0x3498db))
+            return
         rules = ""
         for idx, rule in enumerate(self.rules):
             rules += f"{idx + 1}. {rule}\n"
-        await ctx.send("현재 규칙\n"
-                       f"{rules}")
+        await ctx.send(embed=discord.Embed(title="현재 규칙", description=rules, color=0x3498db))
