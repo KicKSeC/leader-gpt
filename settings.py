08b569592a25f0e2ff8d2eb2e8b118ae88b03d72
Hangyeol Kang <benkang1624@gmail.com>
2023-12-09 23:49:17 +0900

    초기 실행시 events.json파일 검사
    파일이 존재하지 않을시 초기 설정 진행

--- settings.py
+++ settings.py
@@ -5,7 +5,8 @@ import logging
 
 class Settings:
     """사용자로부터 입력에 필요한 데이터를 입력받는 클래스"""
-    path = os.path.join("data", "settings.json")
+    path_setting = os.path.join("data", "settings.json")
+    path_events = os.path.join("data", "events.json")
 
     @classmethod
     def initial_setting(cls):
@@ -13,10 +14,14 @@ class Settings:
         if not os.path.isdir('data'):
             os.mkdir('data')
 
-        if not os.path.isfile(cls.path):
-            with open(cls.path, "w", encoding='utf-8') as f:
+        if not os.path.isfile(cls.path_setting):
+            with open(cls.path_setting, "w", encoding='utf-8') as f:
                 settings = {"channel": None, "members": []}
                 json.dump(settings, f)
+        if not os.path.isfile(cls.path_events):
+            with open(cls.path_events, "w", encoding='utf-8') as f:
+                events = {"assignment": {}, "schedule": []}
+                json.dump(events, f)
 
         logging.basicConfig(filename='data/bot.log', level=logging.DEBUG,  # 로그 파일 설정
                             format='%(asctime)s:%(levelname)s:%(message)s')
@@ -34,16 +39,16 @@ class Settings:
     def save(cls, key, data):
         """setting.json으로부터 채널 정보를 저장함"""
 
-        with open(cls.path, 'r', encoding='utf-8') as f:
+        with open(cls.path_setting, 'r', encoding='utf-8') as f:
             content = json.load(f)
         content[key] = data
-        with open(cls.path, 'w', encoding='utf-8') as f:
+        with open(cls.path_setting, 'w', encoding='utf-8') as f:
             json.dump(content, f, indent=4)
 
     @classmethod
     def load(cls, key: str):
         """setting.json으로부터 채널 정보를 불러옴"""
-        with open(cls.path, 'r', encoding='utf-8') as f:
+        with open(cls.path_setting, 'r', encoding='utf-8') as f:
             data = json.load(f).get(key)
 
         return data
@@ -51,12 +56,12 @@ class Settings:
     @classmethod
     def delete(cls, key: str):
         """주어진 키의 값을 삭제. 성공하면 True, 실패하면 False"""
-        with open(cls.path, 'r', encoding='utf-8') as f:
+        with open(cls.path_setting, 'r', encoding='utf-8') as f:
             content = json.load(f)
         if content.get(key) is None:
             return False
         del content[key]
-        with open(cls.path, 'w', encoding='utf-8') as f:
+        with open(cls.path_setting, 'w', encoding='utf-8') as f:
             json.dump(content, f, indent=4)
 
 
