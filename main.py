781b8af866e10382fcac53ccfd3ca879c5e5cb0f
Hangyeol Kang <benkang1624@gmail.com>
2023-12-11 02:56:28 +0900

    서버 입장시 출력되는 메시지 추가

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 import sys, os
 from discord.ext import commands
 
-from settings import Settings   # 이 클래스가 가장 먼저 임포트되어야 함
+from settings import Settings  # 이 클래스가 가장 먼저 임포트되어야 함
 from bot_command import LGPTCommand
 from commands.user_settings import UserSettings
 from commands.meeting_log import MeetingLog
@@ -12,15 +12,17 @@ from commands.team_rule import TeamRule
 from commands.group_review import GroupReview
 from commands.assignment import Assignment
 from commands.meetingTime import MeetingTime
+
 sys.path.append(os.path.dirname(__file__))
 intents = discord.Intents.default()
 intents.message_content = True
+intents.guilds = True
 intents.members = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 
 @bot.event
-async def on_ready(): 
+async def on_ready():
     await bot.add_cog(LGPTCommand(bot))
     await bot.add_cog(UserSettings(bot))
     await bot.add_cog(MeetingLog(bot))
@@ -32,7 +34,25 @@ async def on_ready():
     assignment_cog = Assignment(bot)
     await bot.add_cog(assignment_cog)
     assignment_cog.check_deadlines.start()  # check_deadlines 시작 - 시연이후 삭제될 코드
-    print(bot.user.name)        # type: ignore
+    print(bot.user.name)  # type: ignore
+
+
+@bot.event
+async def on_guild_join(guild):
+    """서버에 입장했을 때 실행될 코드"""
+    for guild in bot.guilds:
+        default_channel = guild.system_channel  # 디스코드 봇이 들어온 서버의 기본 채널을 가져옵니다.
+        if default_channel is not None:
+            embed = discord.Embed(
+                title="안녕하세요! Leader.GPT입니다!",
+                description="여러분들의 팀장이 될 Leader.GPT라고 합니다\n\n"
+                            "헌신적인 팀장이 될 수 있도록 노력할게요!\n\n"
+                            "!도움말을 입력해 명령어를 확인할 수 있어요!\n\n"
+                            "시작하기에 앞서 기본설정을 위해 '!설정 채널'을 입력해주시겠어요?",
+                color=0x3489db
+            )
+            await default_channel.send(embed=embed)
+            break
 
 
 @bot.event
