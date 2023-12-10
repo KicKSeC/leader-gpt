d781d12b6b57a4262f422654f109776e5370281d
Hangyeol Kang <benkang1624@gmail.com>
2023-12-11 01:53:11 +0900

    익명 평가기능 완성

--- commands/group_review.py
+++ commands/group_review.py
@@ -1,5 +1,6 @@
 ﻿import discord
 from discord.ext import commands
+import statistics as stt
 
 
 class GroupReview(commands.Cog):
@@ -10,21 +11,22 @@ class GroupReview(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.evaluations = {}
+        self.evaluate = False
 
     @commands.group(name="팀원평가")
     async def review_group(self, ctx):
         """'팀원평가' 그룹 커맨드를 정의합니다. 사용자가 '팀원평가' 명령어를 입력하면, 서브 커맨드가 없는 경우에 실행되는 함수입니다."""
         if ctx.invoked_subcommand is None:
             embed = discord.Embed(
-                description="익명 혹은 실명을 선택해주세요. (예: !팀원평가 익명)",
+                description="\'!팀원평가 시작\'을 입력해 익명 팀원평가를 시작 할 수 있습니다.",
                 color=0x3498db
             )
             await ctx.send(embed=embed)
 
-    @review_group.command(name="익명")
+    @review_group.command(name="시작")
     async def review_anonymous(self, ctx):
         """
-        사용자가 '팀원평가 익명' 명령어를 입력하면 실행되는 함수입니다. 익명으로 팀원을 평가하도록 안내합니다.
+        사용자가 '팀원평가 시작' 명령어를 입력하면 실행되는 함수입니다. 익명으로 팀원을 평가하도록 안내합니다.
         """
         await ctx.send(embed=discord.Embed(description="평가를 시작합니다", color=0x3498db))
         guild = ctx.guild
@@ -33,10 +35,12 @@ class GroupReview(commands.Cog):
             if not member.bot:
                 embed = discord.Embed(
                     title='팀원을 평가하세요',
-                    description='평가는 익명으로 진행됩니다',
+                    description='DM으로 \'!평가 팀원명 점수\'를 입력해 팀원을 평가 할 수 있습니다\n예) \'!평가 홍길동 1\'\n'
+                                '또한 \'!평가 홍길동 1 지난번 회의때 참석하지 않음\'과 같이 한줄평을 덧붙일 수 있습니다.',
                     color=0x3498db
                 )
                 await member.send(embed=embed)
+        self.evaluate = True
 
     @review_group.command(name="종료")
     async def finish_review(self, ctx):
@@ -44,23 +48,44 @@ class GroupReview(commands.Cog):
         embed = discord.Embed()
         if not self.evaluations:  # 평가 데이터가 없는 경우, 안내 메시지를 출력하고 함수를 종료합니다.
             embed = discord.Embed(
-                description="저장된 평가가 없습니다"
+                description="저장된 평가가 없습니다",
+                color=0xFF0000
             )
         else:
             result = ""
-            for member_id, evaluation in self.evaluations.items():  # 평가 데이터를 순회하면서, 각 멤버의 평가 결과를 문자열에 추가합니다.
-                member = ctx.guild.get_member(member_id)
-                result += f"{member.display_name}: {evaluation}\n"
+            for member, evaluation in self.evaluations.items():  # 평가 데이터를 순회하면서, 각 멤버의 평가 결과를 문자열에 추가합니다.
+                avg = stt.mean(evaluation['score'])
+                result += f"{member}: {avg}\n"
+                if evaluation['comments']:
+                    for comment in evaluation['comments']:
+                        result += f"- {comment}\n"
+                else:
+                    result += "- 한줄평 없음\n"
             embed = discord.Embed(
-                title="평가 결과",
+                title="---------- 평가 결과 ----------",
                 description=result,
                 color=0x3498db
             )
         await ctx.send(embed=embed)
         self.evaluations = {}
+        self.evaluate = False
 
     @commands.command(name="평가")
-    async def evaluate(self, ctx, *, evaluation):
+    async def evaluate(self, ctx, member=None, score=None, *, comment=None):
         """사용자가 '평가' 명령어를 입력하면 실행되는 함수입니다. 평가 내용을 저장하고, 저장되었다는 메시지를 보냅니다"""
-        self.evaluations[ctx.author.id] = evaluation  # 사용자의 평가 내용을 저장합니다
-        await ctx.author.send(embed=discord.Embed(description="평가가 저장되었습니다", color=0x3498db))
+        # 평가가 시작되었는지 확인합니다
+        if self.evaluate:
+            if member is None or score is None:
+                await ctx.send(embed=discord.Embed(title="잘못된 입력입니다", description="팀원명 혹은 점수가 올바르게 입력되지 않았습니다",
+                                                   color=0xFF0000))
+            score = int(score)
+            if member not in self.evaluations:
+                self.evaluations[member] = {"score": [score], "comments": [comment]}  # 사용자의 평가 내용을 저장합니다
+            else:
+                self.evaluations[member]['score'].append(score)
+                self.evaluations[member]['comments'].append(comment)
+            print(self.evaluations)
+            await ctx.author.send(embed=discord.Embed(description="평가가 저장되었습니다", color=0x3498db))
+        else:
+            await ctx.send(embed=discord.Embed(title="진행되고 있는 평가가 없습니다",
+                                               description="서버에서 !팀원평가 시작을 입력해 평가를 입력해주세요", color=0xFF0000))
